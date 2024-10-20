270c86c0e0287e2f6175cb968ec8df6b38b902b6
6 <88879694+SixOP@users.noreply.github.com>
2024-10-20 15:51:25 +0530

    Update clipboard.js
    added so it will redirect to open mc

--- docs/assets/clipboard.js
+++ docs/assets/clipboard.js
@@ -11,3 +11,9 @@ clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
+
+// Add a click event to open the desired link
+document.querySelector('p#serverIP').addEventListener('click', function() {
+    // Replace 'https://desired-link.com' with the actual link you want to open
+    window.open('minecraft://?addExternalServer=DimensionalMC|DimensionalMC.aternos.me:36035', '_blank'); 
+});
